ce2ea07805cea6474d2f3f1a6cb4ccff4e68574d
datapirateX <67560590+datapirateX@users.noreply.github.com>
2022-10-15 10:05:59 +0600

    Update SongDetails.jsx

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,3 +1,17 @@
-const SongDetails = () => <div>SongDetails</div>;
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
+import { setActiveSong, playPause } from "../redux/features/playerSlice";
+
+
+
+const SongDetails = () => {
+    const { songid } = useParams();
+    
+    console.log(songid);
+
+    return 
+
+}
 
 export default SongDetails;
